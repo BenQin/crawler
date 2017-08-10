380e3bc63627924996a39d3d7155f103fdbe8e07
Ben <465269802@qq.com>
2017-08-10 19:11:22 +0800

    Improve link_crawler.

--- crawl.py
+++ crawl.py
@@ -5,6 +5,8 @@ Crawl website.
 
 import urllib2
 import re
+import urlparse
+import robotparser
 
 def download(url, user_agent='wswp', num_retries=2):
     '''
@@ -36,6 +38,34 @@ def crawl_sitemap(url):
         html = download(link)
         # scrape html here
         # ...
+
+def link_crawler(seed_url, link_regex, user_agent='GoodCrawler'):
+    '''Crawl from the given seed URL following links matched by link_regex
+    '''
+    rp = robotparser.RobotFileParser()
+    link = urlparse.urljoin(seed_url, 'robots.txt')
+    rp.set_url(link)
+    rp.read()
+    crawl_queue = [seed_url]
+    seen = set(crawl_queue)
+    while crawl_queue:
+        url = crawl_queue.pop()
+        if rp.can_fetch(user_agent, url):
+            html = download(url)
+            for link in get_links(html):
+                if re.search(link_regex, link):
+                    link = urlparse.urljoin(seed_url, link)
+                    if link not in seen:
+                        seen.add(link)
+                        crawl_queue.append(link)
+        else:
+            print 'Blocked by robots.txt:', url
+
+def get_links(html):
+    '''Return a list of links from html
+    '''
+    webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
+    return webpage_regex.findall(html)
 
 
 if __name__ == '__main__':
