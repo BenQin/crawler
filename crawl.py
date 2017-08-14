0eeeb6360880545c2df5358da118154d38e7bbd1
ben <465269802@qq.com>
2017-08-14 20:49:24 +0800

    Add crawling with id.

--- crawl.py
+++ crawl.py
@@ -5,8 +5,10 @@ Crawl website.
 
 import urllib2
 import re
+import itertools
 import urlparse
 import robotparser
+
 
 def download(url, user_agent='wswp', num_retries=2):
     '''
@@ -38,6 +40,20 @@ def crawl_sitemap(url):
         html = download(link)
         # scrape html here
         # ...
+
+def crawl_id_traverse():
+    max_errors = 5
+    num_errors = 0
+    for page in itertools.count(1):
+        url = 'http://example.webscraping.com/view/%d' % page
+	html = download(url)
+	if html is None:
+	    num_errors += 1
+	    if num_errors == max_errors:
+	        break
+	else:
+	    num_errors = 0
+	    #print html
 
 def link_crawler(seed_url, link_regex, user_agent='GoodCrawler'):
     '''Crawl from the given seed URL following links matched by link_regex
